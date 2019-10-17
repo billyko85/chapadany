99c1267b17108701c8c5afe3e774a6481eb3763e
billyko85 <dlunati.ko85@gmail.com>
2019-10-17 10:26:18 -0300

    precios Dam

---
+++ MyTeamApp/Dam.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MyTeamApp
+{
+    partial class Dam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.barCastelar = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnLoadDBF = new System.Windows.Forms.Button();
+            this.grdDBF = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDBF)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barCastelar
+            // 
+            this.barCastelar.Location = new System.Drawing.Point(12, 65);
+            this.barCastelar.Name = "barCastelar";
+            this.barCastelar.Size = new System.Drawing.Size(905, 23);
+            this.barCastelar.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Insertar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLoadDBF
+            // 
+            this.btnLoadDBF.Location = new System.Drawing.Point(802, 24);
+            this.btnLoadDBF.Name = "btnLoadDBF";
+            this.btnLoadDBF.Size = new System.Drawing.Size(115, 35);
+            this.btnLoadDBF.TabIndex = 5;
+            this.btnLoadDBF.Text = "Cargar";
+            this.btnLoadDBF.UseVisualStyleBackColor = true;
+            this.btnLoadDBF.Click += new System.EventHandler(this.btnLoadDBF_Click);
+            // 
+            // grdDBF
+            // 
+            this.grdDBF.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDBF.Location = new System.Drawing.Point(12, 94);
+            this.grdDBF.Name = "grdDBF";
+            this.grdDBF.Size = new System.Drawing.Size(905, 385);
+            this.grdDBF.TabIndex = 4;
+            // 
+            // Dam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 508);
+            this.Controls.Add(this.barCastelar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLoadDBF);
+            this.Controls.Add(this.grdDBF);
+            this.Name = "Dam";
+            this.Text = "Dam";
+            ((System.ComponentModel.ISupportInitialize)(this.grdDBF)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar barCastelar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLoadDBF;
+        private System.Windows.Forms.DataGridView grdDBF;
+    }
+}
---
+++ MyTeamApp/Dam.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyTeamApp
+{
+    public partial class Dam : Form
+    {
+        public Dam()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadDBF_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "dBASE files (*.dbf)|*.dbf";
+            ofd.ShowDialog();
+
+            if (ofd.FileName.Length > 0)
+            {
+                DataTable dt = ReadDbf.ReadDBF(ofd.FileName);
+                grdDBF.DataSource = dt;
+
+                Dam_SQLSERVER sql = new Dam_SQLSERVER();
+
+                //sql.InsertarPreciosBulk(dt, barCastelar);
+                sql.InsertarPreciosTrans(dt, barCastelar);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
